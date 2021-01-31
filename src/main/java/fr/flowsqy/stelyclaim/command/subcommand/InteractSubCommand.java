d3a1847008d3a93e4767719cf9d0f75619f4291d
Flo504 <Flo504.Dev@gmail.com>
2021-01-31 17:13:42 +0100

    Implement tp subCommand

---
+++ src/main/java/fr/flowsqy/stelyclaim/command/subcommand/InteractSubCommand.java
@@ -0,0 +1,85 @@
+package fr.flowsqy.stelyclaim.command.subcommand;
+
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import fr.flowsqy.stelyclaim.StelyClaimPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public abstract class InteractSubCommand extends RegionSubCommand {
+
+    public InteractSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean stats, boolean console) {
+        super(plugin, name, alias, permission, stats, console);
+    }
+
+    @Override
+    public void execute(CommandSender sender, List<String> args, int size, boolean isPlayer) {
+        if(size != 1 && size != 2){
+            messages.sendMessage(sender,
+                    "help."
+                            + getName()
+                            + (sender.hasPermission(getPermission()+"-other") ? "-other" : "")
+            );
+            return;
+        }
+        final Player player = (Player) sender;
+
+        final String regionName;
+        final boolean ownRegion;
+        if(size == 1){
+            regionName = player.getName();
+            ownRegion = true;
+        }
+        else{
+            regionName = args.get(1);
+            ownRegion = regionName.equalsIgnoreCase(player.getName());
+        }
+
+        if(!ownRegion && !player.hasPermission(getPermission()+"-other")){
+            messages.sendMessage(player, "help."+getPermission());
+            return;
+        }
+
+        final World world = player.getWorld();
+
+        final RegionManager regionManager = getRegionContainer(world);
+        if(regionManager == null){
+            messages.sendMessage(player,
+                    "claim.worldnothandle",
+                    "%world%", world.getName());
+            return;
+        }
+
+        final ProtectedRegion region = regionManager.getRegion(regionName);
+        if(region == null){
+            messages.sendMessage(player, "claim.notexist" + (ownRegion ? "" : "other"), "%region%", regionName);
+            return;
+        }
+
+        interactRegion(player, region, ownRegion);
+    }
+
+    protected abstract void interactRegion(Player player, ProtectedRegion region, boolean ownRegion);
+
+    @Override
+    public List<String> tab(CommandSender sender, List<String> args, boolean isPlayer) {
+        if(sender.hasPermission(getPermission()+"-other") && args.size() == 2){
+            final String arg = args.get(1).toLowerCase(Locale.ROOT);
+            return Bukkit.getOnlinePlayers().stream()
+                    .filter(((Player) sender)::canSee)
+                    .map(HumanEntity::getName)
+                    .filter(name -> name.toLowerCase(Locale.ROOT).startsWith(arg))
+                    .collect(Collectors.toList());
+        }
+        else
+            return Collections.emptyList();
+    }
+}
--- src/main/java/fr/flowsqy/stelyclaim/command/subcommand/TeleportSubCommand.java
+++ src/main/java/fr/flowsqy/stelyclaim/command/subcommand/TeleportSubCommand.java
@@ -1,22 +1,29 @@
 package fr.flowsqy.stelyclaim.command.subcommand;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
-import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-import java.util.List;
+public class TeleportSubCommand extends InteractSubCommand {
 
-public class TeleportSubCommand extends SubCommand {
     public TeleportSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean stats, boolean console) {
         super(plugin, name, alias, permission, stats, console);
     }
 
     @Override
-    public void execute(CommandSender sender, List<String> args, int size, boolean isPlayer) {
+    protected void interactRegion(Player player, ProtectedRegion region, boolean ownRegion) {
+        final com.sk89q.worldedit.util.Location weLoc = region.getFlag(Flags.TELE_LOC);
 
+        if(weLoc == null){
+            messages.sendMessage(player, "claim.notp" + (ownRegion ? "" : "other"), "%region%", region.getId());
+            return;
+        }
+
+        player.teleport(BukkitAdapter.adapt(weLoc));
+
+        messages.sendMessage(player, "claim.tp" + (ownRegion ? "" : "other"), "%region%", region.getId());
     }
 
-    @Override
-    public List<String> tab(CommandSender sender, List<String> args, boolean isPlayer) {
-        return null;
-    }
 }
