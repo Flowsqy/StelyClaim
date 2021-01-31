ab4417cbd363f725ad01643206479cea7abc8f52
Flo504 <Flo504.Dev@gmail.com>
2021-01-31 19:44:00 +0100

    Fix the move too quickly message

--- src/main/java/fr/flowsqy/stelyclaim/command/subcommand/TeleportSubCommand.java
+++ src/main/java/fr/flowsqy/stelyclaim/command/subcommand/TeleportSubCommand.java
@@ -4,12 +4,21 @@ import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.protection.flags.Flags;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TeleportSubCommand extends InteractSubCommand {
 
+    private final TeleportAsync teleportAsync;
+
     public TeleportSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean stats, boolean console) {
         super(plugin, name, alias, permission, stats, console);
+        teleportAsync = new TeleportAsync();
     }
 
     @Override
@@ -21,9 +30,31 @@ public class TeleportSubCommand extends InteractSubCommand {
             return;
         }
 
-        player.teleport(BukkitAdapter.adapt(weLoc));
+        teleportAsync.addTeleport(player, BukkitAdapter.adapt(weLoc));
 
         messages.sendMessage(player, "claim.tp" + (ownRegion ? "" : "other"), "%region%", region.getId());
+    }
+
+    private final class TeleportAsync {
+
+        private final Map<Player, Location> teleportLocation = new HashMap<>();
+        private final BukkitScheduler scheduler = Bukkit.getScheduler();
+
+        private boolean launched;
+
+        public void addTeleport(Player player, Location location){
+            teleportLocation.put(player, location);
+            if(!launched){
+                scheduler.runTask(plugin, () -> {
+                    for(Map.Entry<Player, Location> entry : teleportLocation.entrySet()){
+                        entry.getKey().teleport(entry.getValue());
+                    }
+                    launched = false;
+                });
+                launched = true;
+            }
+        }
+
     }
 
 }
