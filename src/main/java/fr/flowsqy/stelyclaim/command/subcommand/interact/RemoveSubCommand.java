b9033a002acbcece1be05f88ec5fe67fed4dd221
Flowsqy <Flo504.Dev@gmail.com>
2021-03-14 15:01:25 +0100

    Add pillar message for RemoveSubCommand

--- src/main/java/fr/flowsqy/stelyclaim/command/subcommand/interact/RemoveSubCommand.java
+++ src/main/java/fr/flowsqy/stelyclaim/command/subcommand/interact/RemoveSubCommand.java
@@ -3,13 +3,18 @@ package fr.flowsqy.stelyclaim.command.subcommand.interact;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
+import fr.flowsqy.stelyclaim.util.PillarTextSender;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 
 public class RemoveSubCommand extends InteractSubCommand {
+
+    private final PillarTextSender pillarTextSender;
+
     public RemoveSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean console, List<String> allowedWorlds, boolean statistic) {
         super(plugin, name, alias, permission, console, allowedWorlds, statistic);
+        this.pillarTextSender = new PillarTextSender(messages, "current", plugin.getPillarData());
     }
 
     @Override
@@ -17,6 +22,9 @@ public class RemoveSubCommand extends InteractSubCommand {
         regionManager.removeRegion(region.getId());
 
         messages.sendMessage(player, "claim.command.remove" + (ownRegion ? "" : "-other"), "%region%", regionName);
+
+        pillarTextSender.sendMessage(player, region);
+
         if(!ownRegion)
             plugin.getMailManager().sendInfoToTarget(player, regionName, getName());
     }
