66d9b8d782f2c3172be3e10c8bdf1ac345aeb747
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-10-30 12:59:04 +0100

    Add documentation in ClaimHandler

--- src/main/java/fr/flowsqy/stelyclaim/api/ClaimHandler.java
+++ src/main/java/fr/flowsqy/stelyclaim/api/ClaimHandler.java
@@ -2,16 +2,48 @@ package fr.flowsqy.stelyclaim.api;
 
 public interface ClaimHandler<T extends ClaimOwner> {
 
+    /**
+     * A unique identifier that represent this type of handler
+     *
+     * @return The handler identifier
+     */
     String getId();
 
+    /**
+     * The owner of the claim
+     *
+     * @param claimIdentifier The {@link String} that identifies the owner of the claim
+     * @return The {@link ClaimOwner} of the claim
+     */
     T getOwner(String claimIdentifier);
 
+    /**
+     * Get the {@link String} that identifies the owner of the claim
+     *
+     * @param owner The {@link ClaimOwner} of the claim
+     * @return The {@link String} that identifies the owner of the claim
+     */
     String getIdentifier(T owner);
 
+    /**
+     * Get the {@link RegionModifier} for the Define operation
+     *
+     * @return The Define {@link RegionModifier}
+     */
     RegionModifier<T> getDefineModifier();
 
+    /**
+     * Get the {@link RegionModifier} for the Redefine operation
+     *
+     * @return The Redefine {@link RegionModifier}
+     */
     RegionModifier<T> getRedefineModifier();
 
-    ClaimMessage getMessages();
+    /**
+     * The messages that is used by this type of handler
+     *
+     * @return The {@link FormattedMessages} used by this handler
+     */
+    FormattedMessages getMessages();
 
 }
