84e9c11096dc9c61f48d99d6970504287a157f46
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-11-02 01:13:34 +0100

    Add nearest point and specific name calculation in near sub-command

--- src/main/java/fr/flowsqy/stelyclaim/command/subcommand/NearSubCommand.java
+++ src/main/java/fr/flowsqy/stelyclaim/command/subcommand/NearSubCommand.java
@@ -6,6 +6,9 @@ import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
+import fr.flowsqy.stelyclaim.api.ClaimHandler;
+import fr.flowsqy.stelyclaim.api.ClaimOwner;
+import fr.flowsqy.stelyclaim.api.ProtocolManager;
 import fr.flowsqy.stelyclaim.protocol.RegionFinder;
 import fr.flowsqy.stelyclaim.util.WorldName;
 import org.bukkit.Location;
@@ -19,6 +22,7 @@ import java.util.stream.Collectors;
 
 public class NearSubCommand extends SubCommand {
 
+    private final ProtocolManager protocolManager;
     private final int DEFAULT_DISTANCE;
     private final int DEFAULT_MAX_DISTANCE;
     private final long COOLDOWN;
@@ -34,6 +38,7 @@ public class NearSubCommand extends SubCommand {
         COOLDOWN = configuration.getLong("near.cooldown", 1000L);
         COOLDOWN_SIZE_CLEAR_CHECK = configuration.getInt("cooldown-size-clear-check", 4);
         lastExecTimeByPlayerId = new HashMap<>();
+        protocolManager = plugin.getProtocolManager();
     }
 
     @Override
@@ -90,31 +95,7 @@ public class NearSubCommand extends SubCommand {
             distance = hasFullPerm ? DEFAULT_DISTANCE : Math.min(DEFAULT_DISTANCE, DEFAULT_MAX_DISTANCE);
         }
 
-        // Get the region manager of the world
         final Player player = (Player) sender;
-        final World world = player.getWorld();
-        final RegionManager regionManager = RegionFinder.getRegionManager(new WorldName(world.getName()), player, messages);
-        if (regionManager == null) {
-            return false;
-        }
-
-        final Location pos = player.getLocation();
-        final int x = pos.getBlockX(), z = pos.getBlockZ();
-
-        // Create a region with a radius of distance
-        // Don't check 'y' coordinates
-        final ProtectedCuboidRegion region = new ProtectedCuboidRegion("checking-area",
-                BlockVector3.at(
-                        x + distance,
-                        world.getMaxHeight(),
-                        z + distance
-                ),
-                BlockVector3.at(
-                        x - distance,
-                        world.getMinHeight(),
-                        z - distance
-                )
-        );
 
         // Cooldown of the command
         // Check if cooldown is still active
@@ -135,14 +116,69 @@ public class NearSubCommand extends SubCommand {
             keys.forEach(lastExecTimeByPlayerId::remove);
         }
 
-        final ApplicableRegionSet intersecting = regionManager.getApplicableRegions(region);
-        System.out.println(intersecting);
-        System.out.println(distance);
-        final List<ProtectedRegion> detectedRegions = new LinkedList<>();
-        // TODO
-        // Remove claim where the sender is owner
-        for (ProtectedRegion protectedRegion : intersecting) {
+        // Get the region manager of the world
+        final World world = player.getWorld();
+        final RegionManager regionManager = RegionFinder.getRegionManager(new WorldName(world.getName()), player, messages);
+        if (regionManager == null) {
+            return false;
+        }
 
+        final Location pos = player.getLocation();
+        final int x = pos.getBlockX(), z = pos.getBlockZ();
+
+        // Create a region with a radius of distance
+        // Don't check 'y' coordinates
+        final ProtectedCuboidRegion region = new ProtectedCuboidRegion("checking-area", true,
+                BlockVector3.at(
+                        x + distance,
+                        world.getMaxHeight(),
+                        z + distance
+                ),
+                BlockVector3.at(
+                        x - distance,
+                        world.getMinHeight(),
+                        z - distance
+                )
+        );
+
+
+        // Get the region in the radius
+        final ApplicableRegionSet intersecting = regionManager.getApplicableRegions(region);
+        final List<RegionData> detectedRegions = new LinkedList<>();
+        // Remove claim where the sender is owner and format other
+        for (ProtectedRegion protectedRegion : intersecting) {
+            final String regionId = protectedRegion.getId();
+            final String regionName;
+            // Try to get the ClaimOwner if it's a StelyClaim region
+            if (RegionFinder.isCorrectId(regionId)) {
+                final String[] parts = regionId.split("_", 3);
+                final ClaimHandler<?> regionHandler = protocolManager.getHandler(parts[1]);
+                if (regionHandler == null) {
+                    regionName = regionId;
+                } else {
+                    // Retrieve the ClaimOwner
+                    final ClaimOwner claimOwner = regionHandler.getOwner(parts[2]);
+                    // Check if the player own the region
+                    if (claimOwner.own(player)) {
+                        // Does not display the region that a player own
+                        continue;
+                    }
+                    // Set the proper name of the region
+                    regionName = claimOwner.getName();
+                }
+            } else {
+                regionName = regionId;
+            }
+
+            // Get the nearest point
+            final int[] nearestPoint = getNearestPoint(x, z, protectedRegion);
+            // Get the distance between the nearest point and the player
+            final int nearestX = nearestPoint[0], nearestZ = nearestPoint[1];
+            final int distanceX = x - nearestX;
+            final int distanceZ = z - nearestZ;
+            final double distanceToNearestPoint = Math.sqrt(distanceX * distanceX + distanceZ * distanceZ);
+            // Register the region in the list
+            detectedRegions.add(new RegionData(regionName, nearestX, nearestZ, distanceToNearestPoint));
         }
 
         // TODO
@@ -157,8 +193,64 @@ public class NearSubCommand extends SubCommand {
         return true;
     }
 
+
+    /**
+     * Get the nearest point of the region from a position
+     *
+     * @param posX            The x coordinate of the position
+     * @param posZ            The z coordinate of the position
+     * @param protectedRegion The {@link ProtectedRegion}
+     * @return An {@code int} array of two elements, the x and z coordinate of the nearest point
+     */
+    private int[] getNearestPoint(int posX, int posZ, ProtectedRegion protectedRegion) {
+        final int[] nearestPoint = new int[2];
+        // TODO Better check for polygon region
+        // This check is exact only for cuboid
+        final BlockVector3 max = protectedRegion.getMaximumPoint();
+        final BlockVector3 min = protectedRegion.getMinimumPoint();
+        nearestPoint[0] = getNearestPointOnLine(posX, min.getBlockX(), max.getBlockX());
+        nearestPoint[1] = getNearestPointOnLine(posZ, min.getBlockZ(), max.getBlockZ());
+        return nearestPoint;
+    }
+
+    /**
+     * Get the nearest point on a line from another point
+     *
+     * @param p   The coordinate of the point
+     * @param min The minimum coordinate of the segment
+     * @param max The maximum coordinate of the segment
+     * @return The coordinate of the nearest point
+     */
+    private int getNearestPointOnLine(int p, int min, int max) {
+        // p is too far in positive way
+        if (p > max) {
+            return max;
+        }
+        // p is too far in negative way
+        if (p < min) {
+            return min;
+        }
+        // p is
+        return p;
+    }
+
     @Override
     public List<String> tab(CommandSender sender, List<String> args, boolean isPlayer) {
         return Collections.emptyList();
     }
+
+    private final static class RegionData {
+
+        private final String name;
+        private final int nearestX, nearestZ;
+        private final double distance;
+
+        public RegionData(String name, int nearestX, int nearestZ, double distance) {
+            this.name = name;
+            this.nearestX = nearestX;
+            this.nearestZ = nearestZ;
+            this.distance = distance;
+        }
+    }
+
 }
