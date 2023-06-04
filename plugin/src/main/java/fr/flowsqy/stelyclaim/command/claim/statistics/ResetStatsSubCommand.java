ac621ebd5981a25015f26ab638d2a9b24654e26e
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-04 19:30:10 +0200

    Adapt ResetStatsSubCommand to the new command structure

--- plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/statistics/ResetStatsSubCommand.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/statistics/ResetStatsSubCommand.java
@@ -1,35 +1,46 @@
 package fr.flowsqy.stelyclaim.command.claim.statistics;
 
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
-import fr.flowsqy.stelyclaim.io.StatisticManager;
+import fr.flowsqy.stelyclaim.command.claim.ClaimContextData;
+import fr.flowsqy.stelyclaim.command.struct.CommandContext;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.UUID;
 
 public class ResetStatsSubCommand extends SubStatsSubCommand {
 
-    public ResetStatsSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean console, List<String> allowedWorlds, boolean statistic, StatisticManager statisticManager) {
-        super(plugin, name, alias, permission, console, allowedWorlds, statistic, statisticManager);
+    private final static String NAME = "reset";
+    private final static String[] TRIGGERS = new String[]{NAME, "r"};
+
+    public ResetStatsSubCommand(@NotNull StelyClaimPlugin plugin) {
+        super(NAME, TRIGGERS, plugin);
     }
 
     @Override
-    protected boolean executeSub(CommandSender sender, boolean own, String command, String target) {
+    protected boolean process(@NotNull CommandContext<ClaimContextData> context, boolean own, @Nullable String command, @NotNull OfflinePlayer target) {
         final String other = own ? "" : "-other";
+        final CommandSender sender = context.getSender().getBukkit();
+        final UUID targetId = target.getUniqueId();
+        final String targetName = target.getName();
         if (command == null) {
-            if (statisticManager.remove(target)) {
-                messages.sendMessage(sender, "claim.stats.reset-all" + other, "%target%", target);
-                statisticManager.saveTask();
-                return true;
+            if (!statisticManager.remove(targetId)) {
+                messages.sendMessage(sender, "claim.stats.nodata-all" + other, "%target%", targetName);
+                return false;
             }
-            messages.sendMessage(sender, "claim.stats.nodata-all" + other, "%target%", target);
-        } else {
-            if (statisticManager.removeStat(target, command)) {
-                messages.sendMessage(sender, "claim.stats.reset" + other, "%target%", "%command%", target, command);
-                statisticManager.saveTask();
-                return true;
-            }
-            messages.sendMessage(sender, "claim.stats.nodata" + other, "%target%", "%command%", target, command);
+            messages.sendMessage(sender, "claim.stats.reset-all" + other, "%target%", targetName);
+            statisticManager.saveTask();
+            return true;
         }
-        return false;
+        if (!statisticManager.removeStat(targetId, command)) {
+            messages.sendMessage(sender, "claim.stats.nodata" + other, "%target%", "%command%", targetName, command);
+            return false;
+        }
+        messages.sendMessage(sender, "claim.stats.reset" + other, "%target%", "%command%", targetName, command);
+        statisticManager.saveTask();
+        return true;
     }
+
 }
