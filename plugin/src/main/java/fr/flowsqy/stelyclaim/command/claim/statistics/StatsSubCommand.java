ff5717b658e4bf77a97a9ea6c88bf0f2dbcb86d4
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-04 20:05:26 +0200

    Adapt StatsSubCommand to the new command structure

--- plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/statistics/StatsSubCommand.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/statistics/StatsSubCommand.java
@@ -1,31 +1,27 @@
 package fr.flowsqy.stelyclaim.command.claim.statistics;
 
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
-import fr.flowsqy.stelyclaim.command.claim.OtherSubCommand;
-import fr.flowsqy.stelyclaim.command.claim.SubCommand;
-import fr.flowsqy.stelyclaim.io.StatisticManager;
-import org.bukkit.command.CommandSender;
+import fr.flowsqy.stelyclaim.command.claim.ClaimContextData;
+import fr.flowsqy.stelyclaim.command.claim.HelpMessage;
+import fr.flowsqy.stelyclaim.command.struct.CommandContext;
+import fr.flowsqy.stelyclaim.command.struct.CommandNode;
+import fr.flowsqy.stelyclaim.command.struct.DispatchCommandTabExecutor;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
-public class StatsSubCommand extends OtherSubCommand {
+public class StatsSubCommand extends DispatchCommandTabExecutor<ClaimContextData> implements CommandNode<ClaimContextData> {
 
-    private final ResetStatsSubCommand resetStatsSubCommand;
-    private final ShowStatsSubCommand showStatsSubCommand;
+    private final static String NAME = "stats";
+    private final static String[] TRIGGERS = new String[]{NAME, "s"};
+    private final List<CommandNode<ClaimContextData>> children;
 
-    public StatsSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean console, boolean statistic, StatisticManager statisticManager) {
-        super(
-                plugin.getMessages(),
-                name,
-                alias,
-                permission,
-                console,
-                getAllWorlds(
-                        plugin.getConfiguration().getStringList("worlds.stats.reset"),
-                        plugin.getConfiguration().getStringList("worlds.stats.show")
-                ),
-                statistic
-        );
+    public StatsSubCommand(@NotNull StelyClaimPlugin plugin) {
+        children = Arrays.asList(new ShowStatsSubCommand(plugin), new ResetStatsSubCommand(plugin));
+    }
+
+    /*
         this.resetStatsSubCommand = new ResetStatsSubCommand(
                 plugin,
                 "reset",
@@ -45,65 +41,32 @@ public class StatsSubCommand extends OtherSubCommand {
                 plugin.getConfiguration().getStringList("worlds.stats.show"),
                 statistic,
                 statisticManager
-        );
-    }
+        );*/
 
-    private static List<String> getAllWorlds(List<String> first, List<String> second) {
-        if (first.isEmpty() || second.isEmpty())
-            return new ArrayList<>();
-        final Set<String> worlds = new HashSet<>(first);
-        worlds.addAll(second);
-        return new ArrayList<>(worlds);
-    }
-
-    public void initSubCommands(List<SubCommand> subCommands) {
-        resetStatsSubCommand.initSubCommands(subCommands);
-        showStatsSubCommand.initSubCommands(subCommands);
+    @Override
+    public @NotNull List<CommandNode<ClaimContextData>> getChildren() {
+        return children;
     }
 
     @Override
-    public boolean execute(CommandSender sender, List<String> args, int size, boolean isPlayer) {
-        if (size > 1) {
-            final SubStatsSubCommand subCommand = getSubCommand(args.get(1));
-            if (subCommand != null && sender.hasPermission(subCommand.getPermission()))
-                return subCommand.execute(sender, args, size, isPlayer);
-        }
-        messages.sendMessage(sender, "help." + getName() + (sender.hasPermission(getPermission() + "-other") ? "-other" : ""));
-        return false;
-    }
-
-    private boolean matchSubCommand(SubStatsSubCommand subCommand, String arg) {
-        return subCommand.getName().equalsIgnoreCase(arg) || subCommand.getAlias().equalsIgnoreCase(arg);
-    }
-
-    private SubStatsSubCommand getSubCommand(String arg) {
-        return
-                matchSubCommand(resetStatsSubCommand, arg) ?
-                        resetStatsSubCommand : (
-                        matchSubCommand(showStatsSubCommand, arg) ?
-                                showStatsSubCommand : null
-                );
+    public void fallBackExecute(@NotNull CommandContext<ClaimContextData> context) {
+        // Send help
+        new HelpMessage().sendMessage(context); // TODO Specify stats
     }
 
     @Override
-    public List<String> tab(CommandSender sender, List<String> args, boolean isPlayer) {
-        if (args.size() > 1) {
-            final String arg = args.get(1);
-            final SubStatsSubCommand subCommand = getSubCommand(arg);
-            if (subCommand != null && args.size() > 2 && sender.hasPermission(subCommand.getPermission())) {
-                return subCommand.tab(sender, args, isPlayer);
-            }
-            final List<String> completions = new ArrayList<>();
-            fillList(sender, completions, arg, resetStatsSubCommand);
-            fillList(sender, completions, arg, showStatsSubCommand);
-            return completions;
-        }
-        return Collections.emptyList();
+    public @NotNull String[] getTriggers() {
+        return TRIGGERS;
     }
 
-    private void fillList(CommandSender sender, List<String> completions, String arg, SubStatsSubCommand subCommand) {
-        if (subCommand.getName().startsWith(arg.toLowerCase(Locale.ROOT)) && sender.hasPermission(subCommand.getPermission()))
-            completions.add(subCommand.getName());
+    @Override
+    public @NotNull String getTabCompletion() {
+        return NAME;
+    }
+
+    @Override
+    public boolean canExecute(@NotNull CommandContext<ClaimContextData> context) {
+        return context.hasPermission(getBasePerm());
     }
 
 }
