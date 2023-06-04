37e2201509ddabdf57bc9d15f0f00497e768e3ce
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-04 16:09:53 +0200

    Adapt SelectionSubCommand to new command structure

--- plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/selection/SelectionSubCommand.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/selection/SelectionSubCommand.java
@@ -1,58 +1,19 @@
 package fr.flowsqy.stelyclaim.command.claim.selection;
 
-import fr.flowsqy.stelyclaim.StelyClaimPlugin;
-import fr.flowsqy.stelyclaim.command.claim.ProtocolSubCommand;
-import fr.flowsqy.stelyclaim.internal.PlayerHandler;
-import fr.flowsqy.stelyclaim.internal.PlayerOwner;
-import fr.flowsqy.stelyclaim.util.OfflinePlayerRetriever;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Player;
+import fr.flowsqy.stelyclaim.command.claim.ClaimContextData;
+import fr.flowsqy.stelyclaim.command.claim.interact.InteractSubCommand;
+import fr.flowsqy.stelyclaim.command.struct.CommandContext;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.Collectors;
+public abstract class SelectionSubCommand extends InteractSubCommand {
 
-public abstract class SelectionSubCommand extends ProtocolSubCommand {
-
-    public SelectionSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean console, List<String> allowedWorlds, boolean statistic) {
-        super(plugin, name, alias, permission, console, allowedWorlds, statistic);
+    public SelectionSubCommand(@NotNull String name, @NotNull String[] triggers) {
+        super(name, triggers);
     }
 
     @Override
-    public boolean execute(CommandSender sender, List<String> args, int size, boolean isPlayer) {
-        if (size != 1 && size != 2) {
-            messages.sendMessage(sender,
-                    "help."
-                            + getName()
-                            + (sender.hasPermission(getOtherPermission()) ? "-other" : "")
-            );
-            return false;
-        }
-
-        final Player player = (Player) sender;
-        final OfflinePlayer targetPlayer = size == 1 ? player : OfflinePlayerRetriever.getOfflinePlayer(args.get(1));
-
-        return process(player, protocolManager.getHandler("player"), new PlayerOwner(targetPlayer));
-    }
-
-    protected abstract boolean process(Player player, PlayerHandler handler, PlayerOwner owner);
-
-    @Override
-    public List<String> tab(CommandSender sender, List<String> args, boolean isPlayer) {
-        if (sender.hasPermission(getOtherPermission()) && args.size() == 2) {
-            final String arg = args.get(1).toLowerCase(Locale.ROOT);
-            final Player player = (Player) sender;
-            return Bukkit.getOnlinePlayers().stream()
-                    .filter(player::canSee)
-                    .map(HumanEntity::getName)
-                    .filter(name -> name.toLowerCase(Locale.ROOT).startsWith(arg))
-                    .collect(Collectors.toList());
-        } else
-            return Collections.emptyList();
+    public boolean canExecute(@NotNull CommandContext<ClaimContextData> context) {
+        return context.getSender().isPlayer() && super.canExecute(context);
     }
 
 }
