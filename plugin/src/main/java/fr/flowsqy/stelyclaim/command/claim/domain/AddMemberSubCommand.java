1ce8073385e87946cfc9819facf9b7e245505975
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-04 13:49:32 +0200

    Implement AddMemberSubCommand with new command structure

--- plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/domain/AddMemberSubCommand.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/domain/AddMemberSubCommand.java
@@ -1,23 +1,28 @@
 package fr.flowsqy.stelyclaim.command.claim.domain;
 
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
-import fr.flowsqy.stelyclaim.api.ClaimHandler;
 import fr.flowsqy.stelyclaim.api.ClaimOwner;
+import fr.flowsqy.stelyclaim.api.HandledOwner;
+import fr.flowsqy.stelyclaim.api.ProtocolManager;
+import fr.flowsqy.stelyclaim.api.actor.Actor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
-
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public class AddMemberSubCommand extends DomainSubCommand {
 
-    public AddMemberSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean console, List<String> allowedWorlds, boolean statistic) {
-        super(plugin, name, alias, permission, console, allowedWorlds, statistic);
+    private final static String NAME = "addmember";
+    private final static String[] TRIGGERS = new String[]{NAME, "am"};
+    private final ProtocolManager protocolManager;
+
+    public AddMemberSubCommand(@NotNull StelyClaimPlugin plugin) {
+        super(NAME, TRIGGERS);
+        this.protocolManager = plugin.getProtocolManager();
     }
 
     @Override
-    protected <T extends ClaimOwner> boolean interact(World world, Player sender, ClaimHandler<T> handler, T owner, OfflinePlayer target) {
-        return protocolManager.addMember(world, sender, handler, owner, target);
+    protected <T extends ClaimOwner> boolean interact(@NotNull Actor actor, @NotNull World world, @NotNull HandledOwner<T> owner, @NotNull OfflinePlayer target) {
+        return protocolManager.addMember(world, actor, owner, target);
     }
 
 }
