a90d6314e58e301a8ded7b0e7b509f36884f5c58
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-04 19:35:41 +0200

    Adapt ShowStatsSubCommand to the new command structure

--- plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/statistics/ShowStatsSubCommand.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/statistics/ShowStatsSubCommand.java
@@ -1,45 +1,53 @@
 package fr.flowsqy.stelyclaim.command.claim.statistics;
 
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
-import fr.flowsqy.stelyclaim.io.StatisticManager;
+import fr.flowsqy.stelyclaim.command.claim.ClaimContextData;
+import fr.flowsqy.stelyclaim.command.struct.CommandContext;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.UUID;
 
 public class ShowStatsSubCommand extends SubStatsSubCommand {
 
-    public ShowStatsSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean console, List<String> allowedWorlds, boolean statistic, StatisticManager statisticManager) {
-        super(plugin, name, alias, permission, console, allowedWorlds, statistic, statisticManager);
+    private final static String NAME = "show";
+    private final static String[] TRIGGERS = new String[]{NAME, "s"};
+
+    public ShowStatsSubCommand(@NotNull StelyClaimPlugin plugin) {
+        super(NAME, TRIGGERS, plugin);
     }
 
     @Override
-    protected boolean executeSub(CommandSender sender, boolean own, String command, String target) {
-        if (command == null) {
-            final String other = (own ? "" : "-other");
-            final String path = "claim.stats.show" + other;
-            for (String statCommand : statisticManager.getCommands()) {
-                messages.sendMessage(
-                        sender,
-                        path,
-                        "%command%", "%target%", "%stat%",
-                        statCommand, target, String.valueOf(statisticManager.get(target, statCommand))
-                );
-            }
+    protected boolean process(@NotNull CommandContext<ClaimContextData> context, boolean own, @Nullable String command, @NotNull OfflinePlayer target) {
+        final CommandSender sender = context.getSender().getBukkit();
+        final String targetName = target.getName();
+        final UUID targetId = target.getUniqueId();
+        final String other = (own ? "" : "-other");
+        if (command != null) {
             messages.sendMessage(
                     sender,
-                    "claim.stats.show-total" + other,
-                    "%target%", "%stat%",
-                    target, String.valueOf(statisticManager.getTotal(target))
-            );
-        } else {
-            messages.sendMessage(
-                    sender,
-                    "claim.stats.show" + (own ? "" : "-other"),
+                    "claim.stats.show" + other,
                     "%command%", "%target%", "%stat%",
-                    command, target, String.valueOf(statisticManager.get(target, command))
+                    command, targetName, String.valueOf(statisticManager.get(targetId, command))
             );
         }
-
+        final String path = "claim.stats.show" + other;
+        for (String statCommand : statisticManager.getCommands()) {
+            messages.sendMessage(
+                    sender,
+                    path,
+                    "%command%", "%target%", "%stat%",
+                    statCommand, targetName, String.valueOf(statisticManager.get(targetId, statCommand))
+            );
+        }
+        messages.sendMessage(
+                sender,
+                "claim.stats.show-total" + other,
+                "%target%", "%stat%",
+                targetName, String.valueOf(statisticManager.getTotal(targetId))
+        );
         return true;
     }
 }
