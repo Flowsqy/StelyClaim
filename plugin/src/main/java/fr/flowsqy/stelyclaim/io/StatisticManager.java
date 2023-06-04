48e3434e18e87d9a38af17ce73823173f78821b1
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-04 16:52:11 +0200

    Use Actor and UUID in StatisticManager

--- plugin/src/main/java/fr/flowsqy/stelyclaim/io/StatisticManager.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/io/StatisticManager.java
@@ -1,18 +1,15 @@
 package fr.flowsqy.stelyclaim.io;
 
-import fr.flowsqy.stelyclaim.command.claim.SubCommand;
+import fr.flowsqy.stelyclaim.api.actor.Actor;
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -22,46 +19,58 @@ public class StatisticManager {
     private final File file;
     private final YamlConfiguration config;
     private final Set<String> commands;
-    private final Map<String, Map<String, Integer>> data;
+    private final Map<UUID, Map<String, Integer>> data;
     private final Lock lock;
     private boolean launch = false;
 
-    public StatisticManager(Plugin plugin, File dataFolder) {
+    public StatisticManager(@NotNull Plugin plugin, @NotNull File dataFolder) {
         this.plugin = plugin;
-        this.file = new File(dataFolder, "statistics.yml");
-        this.config = file.exists() ? YamlConfiguration.loadConfiguration(file) : new YamlConfiguration();
-        this.commands = new HashSet<>();
-        this.data = new HashMap<>();
+        file = new File(dataFolder, "statistics.yml");
+        config = file.exists() ? YamlConfiguration.loadConfiguration(file) : new YamlConfiguration();
+        commands = new HashSet<>();
+        data = new HashMap<>();
         lock = new ReentrantLock();
-        initData();
     }
 
     public void initData() {
-        for (Map.Entry<String, Object> entry : config.getValues(false).entrySet()) {
-            if (!(entry.getValue() instanceof ConfigurationSection section))
-                continue;
-            final Map<String, Integer> stats = new HashMap<>();
-            for (Map.Entry<String, Object> commandEntry : section.getValues(false).entrySet()) {
-                final int value;
+        try {
+            lock.lock();
+            data.clear();
+            for (Map.Entry<String, Object> playerEntry : config.getValues(false).entrySet()) {
+                final UUID playerId;
                 try {
-                    value = Integer.parseInt(commandEntry.getValue().toString());
-                } catch (NumberFormatException ignored) {
+                    playerId = UUID.fromString(playerEntry.getKey());
+                } catch (IllegalArgumentException ignored) {
                     continue;
                 }
-                if (commands.contains(commandEntry.getKey())) {
-                    stats.put(commandEntry.getKey(), value);
+                if (!(playerEntry.getValue() instanceof ConfigurationSection section)) {
+                    continue;
+                }
+                final Map<String, Integer> playerStats = new HashMap<>();
+                for (Map.Entry<String, Object> commandEntry : section.getValues(false).entrySet()) {
+                    final int value;
+                    try {
+                        value = Integer.parseInt(commandEntry.getValue().toString());
+                    } catch (NumberFormatException ignored) {
+                        continue;
+                    }
+                    playerStats.put(commandEntry.getKey(), value);
+                }
+                if (!playerStats.isEmpty()) {
+                    data.put(playerId, playerStats);
                 }
             }
-            if (!stats.isEmpty())
-                data.put(entry.getKey(), stats);
+        } finally {
+            lock.unlock();
         }
     }
 
     public void saveTask() {
         try {
             lock.lock();
-            if (launch)
+            if (launch) {
                 return;
+            }
             Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, () -> {
                 try {
                     lock.lock();
@@ -87,86 +96,125 @@ public class StatisticManager {
         }
     }
 
-    private String getPath(String player, String command) {
-        return player + "." + command;
+    private String getPath(@NotNull UUID playerId, @NotNull String command) {
+        return playerId + "." + command;
     }
 
-    public void initSubCommands(Iterable<SubCommand> subCommands) {
-        commands.clear();
-        for (SubCommand subCommand : subCommands) {
-            if (subCommand.isStatistic())
-                commands.add(subCommand.getName());
-        }
-    }
-
-    public int add(CommandSender player, String command) {
+    public void addCommand(@NotNull String command) {
         try {
             lock.lock();
-            if (!commands.contains(command))
-                return -1;
-            final Map<String, Integer> playerData = data.computeIfAbsent(player.getName(), key -> new HashMap<>());
-            final int stat = playerData.merge(command, 1, Integer::sum);
-            config.set(getPath(player.getName(), command), stat);
-            return stat;
+            commands.add(command);
         } finally {
             lock.unlock();
         }
     }
 
-    public int get(String playerName, String command) {
+    public void removeCommand(@NotNull String command) {
         try {
             lock.lock();
-            if (!commands.contains(command))
+            commands.remove(command);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int increment(@NotNull Actor actor, @NotNull String command) {
+        if (!actor.isPlayer()) {
+            return -1;
+        }
+        try {
+            lock.lock();
+            if (!commands.contains(command)) {
                 return -1;
-            final Map<String, Integer> playerData = data.get(playerName);
+            }
+            final UUID playerId = actor.getPlayer().getUniqueId();
+            final Map<String, Integer> playerData = data.computeIfAbsent(playerId, k -> new HashMap<>());
+            final int newStat = playerData.merge(command, 1, Integer::sum);
+            config.set(getPath(playerId, command), newStat);
+            return newStat;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int get(@NotNull Actor actor, @NotNull String command) {
+        if (!actor.isPlayer()) {
+            return -1;
+        }
+        try {
+            lock.lock();
+            if (!commands.contains(command)) {
+                return -1;
+            }
+            final Map<String, Integer> playerData = data.get(actor.getPlayer().getUniqueId());
             return playerData == null ? 0 : playerData.getOrDefault(command, 0);
         } finally {
             lock.unlock();
         }
     }
 
-    public int getTotal(String playerName) {
+    public int getTotal(@NotNull Actor actor) {
+        if (!actor.isPlayer()) {
+            return -1;
+        }
         try {
             lock.lock();
-            final Map<String, Integer> playerData = data.get(playerName);
+            final Map<String, Integer> playerData = data.get(actor.getPlayer().getUniqueId());
             return playerData == null ? 0 : playerData.values().stream().mapToInt(value -> value).sum();
         } finally {
             lock.unlock();
         }
     }
 
-    public boolean remove(String playerName) {
-        try {
-            lock.lock();
-            final boolean result = data.remove(playerName) != null;
-            if (result)
-                config.set(playerName, null);
-            return result;
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    public boolean removeStat(String playerName, String command) {
-        if (!commands.contains(command))
+    public boolean remove(@NotNull Actor actor) {
+        if (!actor.isPlayer()) {
             return false;
+        }
         try {
             lock.lock();
-            final Map<String, Integer> playerData = data.get(playerName);
-            final boolean result = playerData != null && playerData.remove(command) != null;
-            if (result)
-                config.set(getPath(playerName, command), null);
+            final UUID playerId = actor.getPlayer().getUniqueId();
+            final boolean result = data.remove(playerId) != null;
+            if (result) {
+                config.set(playerId.toString(), null);
+            }
             return result;
         } finally {
             lock.unlock();
         }
     }
 
+    public boolean removeStat(@NotNull Actor actor, @NotNull String command) {
+        if (!actor.isPlayer()) {
+            return false;
+        }
+        try {
+            lock.lock();
+            if (!commands.contains(command)) {
+                return false;
+            }
+            final UUID playerId = actor.getPlayer().getUniqueId();
+            final Map<String, Integer> playerData = data.get(playerId);
+            final boolean result = playerData != null && playerData.remove(command) != null;
+            if (result) {
+                config.set(getPath(playerId, command), null);
+            }
+            return result;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    // TODO Check if needed
     public boolean allowStats(String command) {
         return commands.contains(command);
     }
 
-    public Set<String> getCommands() {
-        return commands;
+    public String[] getCommands() {
+        try {
+            lock.lock();
+            return commands.toArray(new String[0]);
+        } finally {
+            lock.unlock();
+        }
     }
 }
