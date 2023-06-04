75466312d5442ebb6e37e8d1cf56c0856a178c12
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-04 14:00:54 +0200

    Implement RemoveOwnerSubCommand with new command structure

--- plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/domain/RemoveOwnerSubCommand.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/domain/RemoveOwnerSubCommand.java
@@ -1,22 +1,28 @@
 package fr.flowsqy.stelyclaim.command.claim.domain;
 
 import fr.flowsqy.stelyclaim.StelyClaimPlugin;
-import fr.flowsqy.stelyclaim.api.ClaimHandler;
 import fr.flowsqy.stelyclaim.api.ClaimOwner;
+import fr.flowsqy.stelyclaim.api.HandledOwner;
+import fr.flowsqy.stelyclaim.api.ProtocolManager;
+import fr.flowsqy.stelyclaim.api.actor.Actor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
-
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public class RemoveOwnerSubCommand extends DomainSubCommand {
-    public RemoveOwnerSubCommand(StelyClaimPlugin plugin, String name, String alias, String permission, boolean console, List<String> allowedWorlds, boolean statistic) {
-        super(plugin, name, alias, permission, console, allowedWorlds, statistic);
+
+    private final static String NAME = "removeowner";
+    private final static String[] TRIGGERS = new String[]{NAME, "ro"};
+    private final ProtocolManager protocolManager;
+
+    public RemoveOwnerSubCommand(@NotNull StelyClaimPlugin plugin) {
+        super(NAME, TRIGGERS);
+        protocolManager = plugin.getProtocolManager();
     }
 
     @Override
-    protected <T extends ClaimOwner> boolean interact(World world, Player sender, ClaimHandler<T> handler, T owner, OfflinePlayer target) {
-        return protocolManager.removeOwner(world, sender, handler, owner, target);
+    protected <T extends ClaimOwner> boolean interact(@NotNull Actor actor, @NotNull World world, @NotNull HandledOwner<T> owner, @NotNull OfflinePlayer target) {
+        return protocolManager.removeOwner(world, actor, owner, target);
     }
 
 }
