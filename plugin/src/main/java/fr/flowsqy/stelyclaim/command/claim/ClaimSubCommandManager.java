cc375111612b8f8d26328883293c4c54f2574702
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-07 20:07:52 +0200

    Implement ClaimSubCommandManager

--- plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/ClaimSubCommandManager.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/command/claim/ClaimSubCommandManager.java
@@ -1,18 +1,41 @@
 package fr.flowsqy.stelyclaim.command.claim;
 
 import fr.flowsqy.stelyclaim.command.struct.CommandNode;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ClaimSubCommandManager {
 
+    // TODO Improve this
+
     private final List<CommandNode<ClaimContextData>> commands;
+    private final List<CommandNode<ClaimContextData>> specifics;
+
+    public ClaimSubCommandManager() {
+        commands = new LinkedList<>();
+        specifics = new LinkedList<>();
+    }
+
+    public void register(@NotNull CommandNode<ClaimContextData> command, boolean isSpecific) {
+        commands.add(command);
+        if (isSpecific) {
+            specifics.add(command);
+        }
+    }
+
+    public void unregister(@NotNull CommandNode<ClaimContextData> command) {
+        commands.remove(command);
+        specifics.remove(command);
+    }
 
     public List<CommandNode<ClaimContextData>> getCommands() {
         return commands;
     }
 
     public List<CommandNode<ClaimContextData>> getSpecifics() {
+        return specifics;
     }
 
 }
