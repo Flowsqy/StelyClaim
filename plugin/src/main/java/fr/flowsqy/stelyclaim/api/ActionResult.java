229c9b016fcd2bdb427f222885dc61e3345bd89d
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-11 22:28:35 +0200

    Add ActionResult

---
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/api/ActionResult.java
@@ -0,0 +1,33 @@
+package fr.flowsqy.stelyclaim.api;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public record ActionResult(int code, boolean success, int codeModifier) {
+
+    public ActionResult(int code, boolean success) {
+        this(code, success, 0);
+    }
+
+    public boolean isModifier(int modifier) {
+        return (codeModifier & modifier) != 0;
+    }
+
+    private final static AtomicInteger actionResultCounter;
+
+    static {
+        actionResultCounter = new AtomicInteger();
+    }
+
+    public static int registerResultCode() {
+        return actionResultCounter.getAndIncrement();
+    }
+
+    public static int getModifier(int... modifiers) {
+        int out = 0;
+        for (int mod : modifiers) {
+            out |= mod;
+        }
+        return out;
+    }
+
+}
