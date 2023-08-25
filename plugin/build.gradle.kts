69981f135d578fb3a6e12bc14c9075039574a7a6
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-08-26 01:37:02 +0200

    Update gradle

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven {
+        url = uri("https://maven.enginehub.org/repo/")
+    }
+    maven {
+        url = uri("https://repo.essentialsx.net/releases/")
+    }
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.3.0-SNAPSHOT")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.5-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.18.1-R0.1-SNAPSHOT")
+    compileOnly("net.essentialsx:EssentialsX:2.20.1")
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+project.base.archivesName = rootProject.name
+group = "fr.flowsqy.stelyclaim"
+version = "3.0.2-SNAPSHOT"
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = rootProject.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+}
+
+tasks.processResources {
+    expand(Pair("version", version))
+}
