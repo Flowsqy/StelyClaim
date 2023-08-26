b604c9bc6e483e7177d9b8ce156734589108397f
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-08-27 00:38:45 +0200

    Reformat TeleportSync

--- plugin/src/main/java/fr/flowsqy/stelyclaim/util/TeleportSync.java
+++ plugin/src/main/java/fr/flowsqy/stelyclaim/util/TeleportSync.java
@@ -4,36 +4,60 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitScheduler;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TeleportSync {
 
     private final Plugin plugin;
-    private final Map<Entity, Location> teleportLocation = new HashMap<>();
-    private final BukkitScheduler scheduler = Bukkit.getScheduler();
-
+    private final Map<UUID, TeleportData> pendingTeleportationMap = new HashMap<>();
+    private final Lock lock;
     private boolean launched;
 
     public TeleportSync(@NotNull Plugin plugin) {
         this.plugin = plugin;
+        lock = new ReentrantLock();
     }
 
-    public void addTeleport(@NotNull Entity player, @NotNull Location location) {
-        teleportLocation.put(player, location);
-        if (!launched) {
-            scheduler.runTask(plugin, () -> {
-                for (Map.Entry<Entity, Location> entry : teleportLocation.entrySet()) {
-                    entry.getKey().teleport(entry.getValue());
-                }
-                teleportLocation.clear();
-                launched = false;
-            });
-            launched = true;
+    public void addTeleport(@NotNull Entity entity, @NotNull Location location) {
+        try {
+            lock.lock();
+            pendingTeleportationMap.put(entity.getUniqueId(), new TeleportData(entity, location));
+            if (!launched) {
+                launchTask();
+            }
+        } finally {
+            lock.unlock();
         }
+    }
+
+    private void launchTask() {
+        launched = true;
+        Bukkit.getScheduler().runTask(plugin, () -> {
+            try {
+                lock.lock();
+                teleportTask();
+            } finally {
+                lock.unlock();
+            }
+            launched = false;
+        });
+    }
+
+    private void teleportTask() {
+        for (TeleportData teleportData : pendingTeleportationMap.values()) {
+            teleportData.entity().teleport(teleportData.entity().getLocation());
+        }
+        pendingTeleportationMap.clear();
+        launched = false;
+    }
+
+    private record TeleportData(@NotNull Entity entity, @NotNull Location location) {
     }
 
 }
